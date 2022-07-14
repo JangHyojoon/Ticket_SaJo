6eb4de4bbed9ed0467d5e1f4017f7f9dd38eda66
seyeonseyeon <103159709+seyeonseyeon@users.noreply.github.com>
2022-07-14 16:58:28 +0900

    jeong

--- Ticket_SaJo/src/main/java/com/multi/controller/MainController_jsy.java
+++ Ticket_SaJo/src/main/java/com/multi/controller/MainController_jsy.java
@@ -1,22 +1,8 @@
 package com.multi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-import lombok.AllArgsConstructor;
-
-@RestController
-@AllArgsConstructor
-@RequestMapping("/ouath")
+@Controller
 public class MainController_jsy {
-	
-	@ResponseBody
-	@GetMapping("/kakao") 
-	
-	public void kakaologin(@RequestParam String code) {
-		System.out.println(code);
-	}
+
 }
